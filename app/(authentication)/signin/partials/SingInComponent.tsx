efb52b1cde5799c58613f997228409e1d185bedd
Pratyush Namdev <pratyushnamdev140@gmail.com>
2025-03-17 14:15:16 +0530

    :zap: update backend route

--- app/(authentication)/signin/partials/SingInComponent.tsx
+++ app/(authentication)/signin/partials/SingInComponent.tsx
@@ -4,7 +4,7 @@ import { FcGoogle } from "react-icons/fc";
 const SignInComponent = () => {
 
   const handleGoogleLogin = () => {
-    window.location.href = `http://localhost:3001/mealbridge/authentication/google?state=donor`;
+    window.location.href = `http://localhost:3001/authentication/google?state=donor`;
   };
 
   return (
