f40451ee58c42a0daf4caeecd5f59b758637f66c
Pratyush Namdev <pratyushnamdev140@gmail.com>
2025-09-01 00:58:46 +0530

    update root page

--- app/(platform)/donate-now/page.tsx
+++ app/(platform)/donate-now/page.tsx
@@ -1,6 +1,6 @@
 import { DonateNowForm } from "./partials";
 import { getDonorProfileServer } from "@lib";
-import type { GetDonorProfileResponseDTO } from "@/api/donor/dto/response/get_donor_profile.dto";
+import type { GetDonorProfileResponseDTO } from "@/apiCalls/donor/dto/response/get_donor_profile.dto";
 
 export default async function DonateNow() {
   const profile: GetDonorProfileResponseDTO | null =
