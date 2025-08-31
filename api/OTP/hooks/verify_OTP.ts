f4aa161dcf8ea3748b64a21b65c423a3a58c5147
Pratyush Namdev <pratyushnamdev140@gmail.com>
2025-09-01 00:27:55 +0530

    :zap: direct call to verifyapi

--- api/OTP/hooks/verify_OTP.ts
+++ api/OTP/hooks/verify_OTP.ts
@@ -1,13 +1,13 @@
 import { useMutation, UseMutationOptions } from "@tanstack/react-query";
 import { VerifyOTPRequestDto } from "../dto/request/verify_OTP.dto";
 import { VerifyOTPResponseDto } from "../dto/response/verify_OTP.dto";
-import OTP_API from "..";
+import { verifyOTP } from "../verify_OTP";
 
 export const useVerifyOTP = (
   options?: UseMutationOptions<VerifyOTPResponseDto, Error, VerifyOTPRequestDto>
 ) => {
   return useMutation({
-    mutationFn: OTP_API.verifyOTP,
+    mutationFn: verifyOTP,
     ...options,
   });
 };
