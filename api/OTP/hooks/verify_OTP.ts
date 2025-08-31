dd6a2797410674e7831d05aaaea49f48d4a79c09
Pratyush Namdev <pratyushnamdev140@gmail.com>
2025-09-01 00:15:39 +0530

    change in otp

--- api/OTP/hooks/verify_OTP.ts
+++ api/OTP/hooks/verify_OTP.ts
@@ -1,7 +1,7 @@
 import { useMutation, UseMutationOptions } from "@tanstack/react-query";
 import { VerifyOTPRequestDto } from "../dto/request/verify_OTP.dto";
 import { VerifyOTPResponseDto } from "../dto/response/verify_OTP.dto";
-import OTP_API from "../index";
+import OTP_API from "..";
 
 export const useVerifyOTP = (
   options?: UseMutationOptions<VerifyOTPResponseDto, Error, VerifyOTPRequestDto>
